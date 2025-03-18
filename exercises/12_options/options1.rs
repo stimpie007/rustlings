3f11bc07d7bf7954d8a6ef3867c2dcf8deb08d37
stimpie007 <stimpie007@github.com>
2025-03-18 16:57:53 +1300

    feat: progress for options1.rs

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,13 @@
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day < 22 {
+        Some(5)
+    } else if hour_of_day > 24 {
+         None
+    } else {
+        Some(0)
+    }
 }
 
 fn main() {

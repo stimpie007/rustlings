50c2005792ee764f54bff9a63418b168d32ea244
stimpie007 <stimpie007@github.com>
2025-03-18 16:49:07 +1300

    feat: solutions for hashmaps

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,15 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        // Update Team 1's scores
+        let entry = scores.entry(team_1_name).or_default();
+        entry.goals_scored += team_1_score;
+        entry.goals_conceded += team_2_score;
+
+        // Update Team 2's scores
+        let entry = scores.entry(team_2_name).or_default();
+        entry.goals_scored += team_2_score;
+        entry.goals_conceded += team_1_score;
     }
 
     scores
